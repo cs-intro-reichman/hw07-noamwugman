8841cc6639f7127587f01a4619067ac15db1efc8
noamwugman <noamwug24@gmail.com>
2025-12-27 13:14:17 +0200

    noam

--- Koch.java
+++ Koch.java
@@ -8,35 +8,56 @@ public class Koch {
 		//// Uncomment only one block in each test, and remember to compile
 		//// the class whenever you change the test.
 
-        /*
+        
 		// Tests the curve function:
 		// Gets n, x1, y1, x2, y2,
 		// and draws a Koch curve of depth n from (x1,y1) to (x2,y2).
-		curve(Integer.parseInt(args[0]),
+		/*curve(Integer.parseInt(args[0]),
 			  Double.parseDouble(args[1]), Double.parseDouble(args[2]), 
 		      Double.parseDouble(args[3]), Double.parseDouble(args[4]));
 		*/
 
-		/*
+		
 		// Tests the snowflake function:
 		// Gets n, and draws a Koch snowflake of n edges in the standard canvass.
 		snowFlake(Integer.parseInt(args[0]));
-		*/
+		
 	}
 
 	/** Gets n, x1, y1, x2, y2,
      *  and draws a Koch curve of depth n from (x1,y1) to (x2,y2). */
 	public static void curve(int n, double x1, double y1, double x2, double y2) {
-		StdDraw.line(x1, y1, x2, y2);
-		//// Write the rest of your code below.
+		double x3 = (Math.sqrt(3) / 6.0) * (y1 - y2) + 0.5 * (x1 + x2);
+		double y3 = (Math.sqrt(3) / 6.0) * (x2 - x1) + 0.5 * (y1 + y2);
+		double x12 = x1 + ((x2 - x1) / 3);
+    	double x21 = x2 - ((x2 - x1) / 3);
+    	double y12 = y1 + ((y2 - y1) / 3);
+    	double y21 = y2 - ((y2 - y1) / 3);
+		if (n == 0) {
+			StdDraw.line(x1, y1, x2, y2);
+			StdDraw.setPenColor(StdDraw.WHITE);
+			StdDraw.line(x12, y12, x21, y21);
+			StdDraw.setPenColor(StdDraw.BLACK);
+			StdDraw.line(x1, y1, x3, y3);
+			StdDraw.line(x3, y3, x2, y2);
+		}
+		else {
+			curve(n - 1, x1, y1, x12, y12);
+			curve(n - 1, x12, y12, x3, y3);
+			curve(n - 1, x3, y3, x21, y21);
+			curve(n - 1, x21, y21, x2, y2);
+		}
 	}
 
     /** Gets n, and draws a Koch snowflake of n edges in the standard canvass. */
 	public static void snowFlake(int n) {
 		// A little tweak that makes the drawing look better
-		StdDraw.setYscale(0, 1.1);
-		StdDraw.setXscale(0, 1.1);
-		// Draws a Koch snowflake of depth n
-		//// Write the rest of your code below.
+		StdDraw.setXscale(-0.5, 1.5);
+		StdDraw.setYscale(-0.5, 1.5);
+		double[] xVertex = { 0.000, 1.000, 0.500 };
+		double[] yVertex = { 1.000, 1.000, 1-0.866 };
+		curve(n, xVertex[0], yVertex[0], xVertex[1], yVertex[1]);
+		curve(n, xVertex[1], yVertex[1], xVertex[2], yVertex[2]);
+		curve(n, xVertex[2], yVertex[2], xVertex[0], yVertex[0]);
 	}
 }
--- Sierpinski.java
+++ Sierpinski.java
@@ -7,12 +7,31 @@ public class Sierpinski {
 	
 	// Draws a Sierpinski triangle of depth n on the standard canvass.
 	public static void sierpinski (int n) {
-		//// Replace this comment with your code
+		StdDraw.setCanvasSize(800,800);
+		StdDraw.setXscale(-0.05, 1.05);
+		StdDraw.setYscale(-0.05, 1.05);
+		double[] xVertex = { 0.000, 1.000, 0.500 };
+		double[] yVertex = { 0.000, 0.000, 0.866 };
+		sierpinski(n, xVertex[0], xVertex[1], xVertex[2], yVertex[0], yVertex[1], yVertex[2]);
 	}
 	
 	// Does the actual drawing, recursively.
-	private static void sierpinski(int n, double x1, double x2, double x3,
-		                                 double y1, double y2, double y3) {
-		//// Replace this comment with your code
+	private static void sierpinski(int n, double x1, double x2, double x3, double y1, double y2, double y3) {
+		if (n == 0) {
+			StdDraw.line(x1, y1, x2, y2);
+			StdDraw.line(x2, y2, x3, y3);
+			StdDraw.line(x3, y3, x1, y1);
+		}
+		else {
+			StdDraw.line(x1, y1, x2, y2);
+			StdDraw.line(x2, y2, x3, y3);
+			StdDraw.line(x3, y3, x1, y1);
+			double[] mid12 = {(x1 + x2) / 2, (y1 + y2) / 2};
+			double[] mid23 = {(x2 + x3) / 2, (y2 + y3) / 2};
+			double[] mid31 = {(x3 + x1) / 2, (y3 + y1) / 2};
+			sierpinski(n-1, x1, mid12[0], mid31[0], y1, mid12[1], mid31[1]);
+			sierpinski(n-1, x2, mid12[0], mid23[0], y2, mid12[1], mid23[1]);
+			sierpinski(n-1, x3, mid23[0], mid31[0], y3, mid23[1], mid31[1]);
+		}
 	}
 }
